c498875c77dc472e4c0b3ffc5fa0726376629655
Mark Henmi <mrhenmi@gmail.com>
2026-02-10 21:44:37 -0800

    did a little work not complete

---
+++ ast/ast.go
@@ -0,0 +1,48 @@
+package ast
+
+import (
+	"github.com/mrhenmi/interpreter_in_go/token"
+)
+
+type Node interface {
+	TokenLiteral() string
+}
+
+type Statement interface {
+	Node
+	statementNode()
+}
+
+type Expression interface {
+	Node
+	expressionNode()
+}
+
+type Program struct {
+	Statement []Statement
+}
+
+func (p *Program) TokenLiteral() string {
+	if len(p.Statement) > 0 {
+		return p.Statement[0].TokenLiteral()
+	} else {
+		return ""
+	}
+}
+
+type LetStatemen struct {
+	Token token.Token
+	Name  *Identifier
+	Value Expression
+}
+
+func (ls *LetStatemen) statementNode()       {}
+func (ls *LetStatemen) TokenLiteral() string { return ls.Token.Literal }
+
+type Identifier struct {
+	Token token.Token
+	Value string
+}
+
+func (i *Identifier) expressionNode()      {}
+func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
